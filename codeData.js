1af3a8f199b2abf9d682aaa696a897c6b5fef76e
igfernandezTCCO <igfernandez@tcco.com>
2025-05-02 14:55:44 -0400

    Add files via upload

---
+++ codeData.js
@@ -0,0 +1,164 @@
+const codeData = {
+  "California": {
+    "buildingCode": "2022 CA Building Code, Title 24",
+    "modelCode": "IBC 2021",
+    "asce7": "ASCE 7-16 + Supplements 1, 2, & 3",
+    "aisc": "AISC 360-16 (15th Ed, blue book)",
+    "aiscSeismic": "AISC 341-16 (3rd Ed, blue book)",
+    "aciTms": "TMS 402/602-16",
+    "nds": "NDS-18 / SDPWS-21"
+  },
+  "Colorado": {
+    "buildingCode": "No statewide code (IBC 2021 for state projects)",
+    "modelCode": "IBC 2021",
+    "asce7": "ASCE 7-16 + Supplement No.1",
+    "aisc": "AISC 360-16",
+    "aiscSeismic": "AISC 341-16 + Supplement 1",
+    "aciTms": "TMS 402/602-16",
+    "nds": "NDS-18 / SDPWS-21"
+  },
+  "Connecticut": {
+    "buildingCode": "2022 Connecticut State Building Code",
+    "modelCode": "IBC 2021",
+    "asce7": "ASCE 7-16 + Supplement No.1",
+    "aisc": "AISC 360-16",
+    "aiscSeismic": "AISC 341-16 + Supplement 1",
+    "aciTms": "TMS 402/602-16",
+    "nds": "NDS-18 / SDPWS-21"
+  },
+  "Florida": {
+    "buildingCode": "2023 Florida Building Code, 8th Edition",
+    "modelCode": "IBC 2021",
+    "asce7": "ASCE 7-22",
+    "aisc": "AISC 360-16",
+    "aiscSeismic": "AISC 341-16",
+    "aciTms": "TMS 402/602-16",
+    "nds": "NDS-18 / SDPWS-21"
+  },
+  "Illinois": {
+    "buildingCode": "No adopted statewide code",
+    "modelCode": "IBC 2018 (Chicago)",
+    "asce7": "ASCE 7-16",
+    "aisc": "AISC 360-16",
+    "aiscSeismic": "AISC 341-16",
+    "aciTms": "TMS 402/602-16",
+    "nds": "NDS-18 / SDPWS-15"
+  },
+  "Indiana": {
+    "buildingCode": "2014 Indiana Building Code",
+    "modelCode": "IBC 2012",
+    "asce7": "ASCE 7-10",
+    "aisc": "AISC 360-10",
+    "aiscSeismic": "AISC 341-10",
+    "aciTms": "ACI 530-11",
+    "nds": "NDS-12"
+  },
+  "Louisiana": {
+    "buildingCode": "Louisiana State Uniform Construction Code",
+    "modelCode": "IBC 2021",
+    "asce7": "ASCE 7-16 + Supplement No.1",
+    "aisc": "AISC 360-16",
+    "aiscSeismic": "AISC 341-16 + Supplement 1",
+    "aciTms": "TMS 402/602-16",
+    "nds": "NDS-18 / SDPWS-21"
+  },
+  "Maine": {
+    "buildingCode": "MUBEC (2018)",
+    "modelCode": "IBC 2015",
+    "asce7": "ASCE 7-10 + Supplement No. 1",
+    "aisc": "AISC 360-10",
+    "aiscSeismic": "AISC 341-10",
+    "aciTms": "ACI 530-13",
+    "nds": "NDS-15"
+  },
+  "Massachusetts": {
+    "buildingCode": "MA State Building Code 780, 10th Ed",
+    "modelCode": "IBC 2021",
+    "asce7": "ASCE 7-16 + Supplement No.1",
+    "aisc": "AISC 360-16",
+    "aiscSeismic": "AISC 341-16 + Supplement 1",
+    "aciTms": "TMS 402/602-16",
+    "nds": "NDS-18 / SDPWS-21"
+  },
+  "Michigan": {
+    "buildingCode": "2021 Michigan Building Code",
+    "modelCode": "IBC 2021",
+    "asce7": "ASCE 7-16 + Supplement No.1",
+    "aisc": "AISC 360-16",
+    "aiscSeismic": "AISC 341-16 + Supplement 1",
+    "aciTms": "TMS 402/602-16",
+    "nds": "NDS-18 / SDPWS-21"
+  },
+  "New York": {
+    "buildingCode": "2020 Building Code of NYS",
+    "modelCode": "IBC 2018",
+    "asce7": "ASCE 7-16",
+    "aisc": "AISC 360-16",
+    "aiscSeismic": "AISC 341-16",
+    "aciTms": "TMS 402/602-16",
+    "nds": "NDS-18 / SDPWS-15"
+  },
+  "North Carolina": {
+    "buildingCode": "2018 North Carolina State Building Code",
+    "modelCode": "IBC 2018",
+    "asce7": "ASCE 7-16",
+    "aisc": "AISC 360-16",
+    "aiscSeismic": "AISC 341-16",
+    "aciTms": "TMS 402/602-16",
+    "nds": "NDS-18 / SDPWS-15"
+  },
+  "Ohio": {
+    "buildingCode": "2024 Ohio Building Code",
+    "modelCode": "IBC 2021",
+    "asce7": "ASCE 7-16 + Supplement No.1",
+    "aisc": "AISC 360-16",
+    "aiscSeismic": "AISC 341-16",
+    "aciTms": "TMS 402/602-16",
+    "nds": "NDS-18 / SDPWS-21"
+  },
+  "Oregon": {
+    "buildingCode": "2022 Oregon Structural Specialty Code",
+    "modelCode": "IBC 2021",
+    "asce7": "ASCE 7-16 + Supplements 1, 2, & 3",
+    "aisc": "AISC 360-16",
+    "aiscSeismic": "AISC 341-16",
+    "aciTms": "TMS 402/602-16",
+    "nds": "NDS-18 / SDPWS-21"
+  },
+  "Pennsylvania": {
+    "buildingCode": "PA Uniform Construction Code (UCC)",
+    "modelCode": "IBC 2018",
+    "asce7": "ASCE 7-16",
+    "aisc": "AISC 360-16",
+    "aiscSeismic": "AISC 341-16",
+    "aciTms": "TMS 402/602-16",
+    "nds": "NDS-18 / SDPWS-15"
+  },
+  "Texas": {
+    "buildingCode": "IBC 2012",
+    "modelCode": "IBC 2012",
+    "asce7": "ASCE 7-10",
+    "aisc": "AISC 360-10",
+    "aiscSeismic": "AISC 341-10",
+    "aciTms": "ACI 530-11",
+    "nds": "NDS-12"
+  },
+  "Virginia": {
+    "buildingCode": "2021 Virginia Construction Code",
+    "modelCode": "IBC 2021",
+    "asce7": "ASCE 7-22",
+    "aisc": "AISC 360-16",
+    "aiscSeismic": "AISC 341-16",
+    "aciTms": "TMS 402/602-16",
+    "nds": "NDS-18 / SDPWS-21"
+  },
+  "Washington": {
+    "buildingCode": "2021 Washington State Building Code",
+    "modelCode": "IBC 2021",
+    "asce7": "ASCE 7-16 + Supplements 1, 2, & 3",
+    "aisc": "AISC 360-16",
+    "aiscSeismic": "AISC 341-16",
+    "aciTms": "TMS 402/602-16",
+    "nds": "NDS-18 / SDPWS-21"
+  }
+};
