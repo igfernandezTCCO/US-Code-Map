8cfd9bf9f51a468cc64dc8756946feb6bff56471
igfernandezTCCO <igfernandez@tcco.com>
2025-05-05 09:00:06 -0400

    Update codeData.js

--- codeData.js
+++ codeData.js
@@ -89,13 +89,13 @@
     "nds": "NDS-18 / SDPWS-21"
   },
   "New Jersey": {
-  buildingCode: "2021 IBC NJ Edition",
-  modelCode: "IBC 2021",
-  asce7: "ASCE 7-16 + Supplement No.1",
-  aisc: "AISC 360-16 (15th Ed, blue book)",
-  aiscSeismic: "AISC 341-16 + Supplement 1 (3rd Ed, blue book)",
-  aciTms: "TMS 402/602-16",
-  nds: "NDS-18 / SDPWS-21"
+  "buildingCode": "2021 IBC NJ Edition",
+  "modelCode"": "IBC 2021",
+  "asce7": "ASCE 7-16 + Supplement No.1",
+  "aisc": "AISC 360-16 (15th Ed, blue book)",
+  "aiscSeismic": "AISC 341-16 + Supplement 1 (3rd Ed, blue book)",
+  "aciTms": "TMS 402/602-16",
+  "nds": "NDS-18 / SDPWS-21"
 },
   "New York": {
     "buildingCode": "2020 Building Code of NYS",
